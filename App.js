23bb35edcd59a983ffd82ed70519ec0e37cac7bd
Ketan <ketantoo@gmail.com>
2020-08-27 03:24:14 +0530

    initial commit

--- App.js
+++ App.js
@@ -3,9 +3,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("app executed");
+  var x;
+  // x.toString();
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on yo !</Text>
       <StatusBar style="auto" />
     </View>
   );
