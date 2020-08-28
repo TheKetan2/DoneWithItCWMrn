02e9aeb792e5d9186228e4045070df84b263fee9
Ketan <ketantoo@gmail.com>
2020-08-29 01:45:02 +0530

    touchable components

--- App.js
+++ App.js
@@ -1,24 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+} from "react-native";
 
 export default function App() {
   console.log("app executed");
-  var x;
-  // x.toString();
+  const onPressed = () => console.log("text pressed fn");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on yo !</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={onPressed}>
+        Open up App.js to start
+      </Text>
+      <TouchableNativeFeedback onPress={onPressed}>
+        {/* <Image
+          blurRadius={1}
+          fadeDuration={1000}
+          source={{
+            uri: "https://picsum.photos/200/300",
+            height: 300,
+            width: 200,
+          }}
+        /> */}
+        <View
+          style={{ width: 200, height: 50, backgroundColor: "dodgerblue" }}
+        ></View>
+      </TouchableNativeFeedback>
       <StatusBar style="auto" />
-    </View>
+      {/* </View> */}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
