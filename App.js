7ad7ecb1afeb1dbc5ba7b0d2ccd087c3a69ece1a
Ketan <ketantoo@gmail.com>
2020-09-07 10:14:21 +0530

    starting advanced section

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import * as ImagePicker from "expo-image-picker";
 import { View, Text, TextInput, Switch } from "react-native";
 
 import Screen from "./app/components/Screen";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+    if (!granted) {
+      alert("You need permission");
+    }
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
   return (
     <Screen>
       <ListingEditScreen />
