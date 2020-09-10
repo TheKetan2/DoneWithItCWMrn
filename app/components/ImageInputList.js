2b8774075c6b9eee07177052d2f25136f0a0b1d4
Ketan <ketantoo@gmail.com>
2020-09-10 22:56:32 +0530

    useRef hook used to move the curser to the last ImageInput component when new image added or existing image removed using scrollToEnd method

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,18 +1,31 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useRef } from "react";
+import { StyleSheet, View, ScrollView } from "react-native";
 
 import ImageInput from "./ImageInput";
 
 const ImageInputList = ({ imageURIs = [], onRemoveImage, onAddImage }) => {
-  return (
-    <View style={styles.conatainer}>
-      {imageURIs.map((uri) => (
-        <View key={uri} style={styles.image}>
-          <ImageInput imageURI={uri} onChangeImage={() => onRemoveImage(uri)} />
-        </View>
-      ))}
+  const scrollView = useRef();
 
-      <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
+  return (
+    <View>
+      <ScrollView
+        horizontal
+        ref={scrollView}
+        onContentSizeChange={() => scrollView.current.scrollToEnd()}
+      >
+        <View style={styles.conatainer}>
+          {imageURIs.map((uri) => (
+            <View key={uri} style={styles.image}>
+              <ImageInput
+                imageURI={uri}
+                onChangeImage={() => onRemoveImage(uri)}
+              />
+            </View>
+          ))}
+
+          <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
+        </View>
+      </ScrollView>
     </View>
   );
 };
