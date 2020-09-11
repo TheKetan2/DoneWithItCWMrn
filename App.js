567bd19c7d093683bb826771dabba9acf960b5d6
Ketan <ketantoo@gmail.com>
2020-09-12 01:53:49 +0530

    Created AuthNavigator for login flow

--- App.js
+++ App.js
@@ -3,85 +3,12 @@ import { Image, Text, TouchableOpacity, View, Button } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-
-import Screen from "./app/components/Screen";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-import ListingScreen from "./app/screens/ListingsScreen";
-
-const Stack = createStackNavigator();
-const StackNavigator = () => (
-  <Stack.Navigator
-    screenOptions={{
-      headerStyle: {
-        backgroundColor: "tomato",
-      },
-      headerTintColor: "white",
-    }}
-  >
-    <Stack.Screen name="Tweets" component={Tweets} />
-
-    <Stack.Screen
-      name="TweetDetails"
-      component={TweetsDetails}
-      options={{
-        headerStyle: {
-          backgroundColor: "tomato",
-        },
-        headerTintColor: "white",
-      }}
-    />
-  </Stack.Navigator>
-);
-
-const Tab = createBottomTabNavigator();
-const TabNavigator = () => (
-  <Tab.Navigator
-    tabBarOptions={{
-      activeBackgroundColor: "tomato",
-      activeTintColor: "white",
-      inactiveBackgroundColor: "#eee",
-      inactiveTintColor: "black",
-      size: 25,
-    }}
-  >
-    <Tab.Screen
-      options={{
-        tabBarIcon: ({ size, color }) => (
-          <MaterialCommunityIcons name={"home"} size={size} color={color} />
-        ),
-      }}
-      name="Home"
-      component={ListingScreen}
-    />
-
-    <Tab.Screen
-      options={{
-        tabBarIcon: ({ size, color }) => (
-          <MaterialCommunityIcons name={"plus"} size={size} color={color} />
-        ),
-      }}
-      name="Add"
-      component={ListingEditScreen}
-    />
-
-    <Tab.Screen
-      options={{
-        tabBarIcon: ({ size, color }) => (
-          <MaterialCommunityIcons name={"account"} size={size} color={color} />
-        ),
-      }}
-      name="Account"
-      component={AccountScreen}
-    />
-  </Tab.Navigator>
-);
+import AuthNavigator from "./app/navigation/AuthNavigator";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <TabNavigator />
+      <AuthNavigator />
     </NavigationContainer>
   );
 }
