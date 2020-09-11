6a2baf916a4b9324fd1cf64794929fbba37e3022
Ketan <ketantoo@gmail.com>
2020-09-11 14:08:32 +0530

    using useNavigation hooks

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Image, Text, TouchableOpacity, View, Button } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 
 import Screen from "./app/components/Screen";
 import ImageInput from "./app/components/ImageInput";
@@ -9,9 +9,20 @@ import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 import ImageInputList from "./app/components/ImageInputList";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 
-const Tweets = () => (
+const Link = () => {
+  const navigation = useNavigation();
+  return (
+    <Button
+      title={"Link"}
+      onPress={() => navigation.navigate("TweetDetails")}
+    />
+  );
+};
+
+const Tweets = ({ navigation }) => (
   <Screen>
     <Text>Tweets</Text>
+    <Link />
   </Screen>
 );
 
