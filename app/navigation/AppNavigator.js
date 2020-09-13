13d8a6e3b5fd31a7cfe29a1d7728a222f2fdefd4
Ketan <ketantoo@gmail.com>
2020-09-13 21:36:33 +0530

    set up AccountNavigator

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -5,14 +5,15 @@ import ListingScreen from "../screens/ListingsScreen";
 import AccountScreen from "../screens/AccountScreen";
 import ListingEditScreen from "../screens/ListingEditScreen";
 import FeedStackNavigator from "./FeedNavigator";
+import AccountStackNavigator from "./AccountNavigator";
 
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => (
-  <Tab.Navigator>
+  <Tab.Navigator mode="card">
     <Tab.Screen name="Feed" component={FeedStackNavigator}></Tab.Screen>
     <Tab.Screen name="ListingEdit" component={ListingEditScreen}></Tab.Screen>
-    <Tab.Screen name="Account" component={AccountScreen}></Tab.Screen>
+    <Tab.Screen name="Account" component={AccountStackNavigator}></Tab.Screen>
   </Tab.Navigator>
 );
 
