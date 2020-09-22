796babde60070981e55da6df63d19b7bd657dfea
Ketan <ketantoo@gmail.com>
2020-09-22 21:16:25 +0530

    used context to pass user object

--- frontend/App.js
+++ frontend/App.js
@@ -7,17 +7,17 @@ import AppNavigator from "./app/navigation/AppNavigator";
 import AuthNavigator from "./app/navigation/AuthNavigator";
 
 import OfflineNotice from "./app/components/OfflineNotice";
+import AuthContext from "./app/auth/context";
 
 export default function App() {
   const [user, setUser] = useState();
   return (
-    <>
+    <AuthContext.Provider value={{ user, setUser }}>
       <OfflineNotice />
-      {/* <Text>HihI</Text> */}
+
       <NavigationContainer theme={navigationTheme}>
-        {/* <AppNavigator /> */}
-        <AuthNavigator />
+        {user ? <AppNavigator /> : <AuthNavigator />}
       </NavigationContainer>
-    </>
+    </AuthContext.Provider>
   );
 }
