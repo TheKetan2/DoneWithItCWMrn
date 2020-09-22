bc206a6e69317de864a65858b528048ad011d419
Ketan <ketantoo@gmail.com>
2020-09-22 20:45:01 +0530

    added jwtDecode  for parsing jwt

--- frontend/App.js
+++ frontend/App.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { Text } from "react-native";
 
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
+import AuthNavigator from "./app/navigation/AuthNavigator";
+
 import OfflineNotice from "./app/components/OfflineNotice";
 
 export default function App() {
+  const [user, setUser] = useState();
   return (
     <>
       <OfflineNotice />
       {/* <Text>HihI</Text> */}
       <NavigationContainer theme={navigationTheme}>
-        <AppNavigator />
+        {/* <AppNavigator /> */}
+        <AuthNavigator />
       </NavigationContainer>
     </>
   );
